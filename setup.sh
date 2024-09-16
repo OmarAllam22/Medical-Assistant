6f4617d75a3e78aec9cc107eee03c1011d955c17
omarallam22 <omar22allam@gmail.com>
2024-09-16 11:05:19 +0000

    organizing files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install requirements.txt
 chmod +x main_script.sh
-python tools/create_vectordb.py
-python tools/get_books_info.py
+python utils/create_vectordb.py
+python utils/get_books_info.py
